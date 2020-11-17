0eef091e06febf0bb58571cc7fc2f356a56e905c
Vitor <vesctor2@gmail.com>
2020-11-17 09:52:41 -0300

    atualização

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public num1 : number;
+  public num2 : number;
 
+  public operacao: string;
+
+  constructor(public alertController: AlertController) {}
+
+  async vaiFormulario() {
+
+    console.log(this.operacao)
+
+    //template strings!
+    let total = this.num1 + this.num2;
+    const texto = `O valor total da soma é : ${total}`
+
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: texto,
+      buttons: ['Vai']
+    });
+    await alert.present();
+  }
 }
